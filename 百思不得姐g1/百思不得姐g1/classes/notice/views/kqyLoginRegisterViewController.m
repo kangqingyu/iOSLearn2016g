9dcf9a5ce62a18acfea3f729caffab6ae57d92e5
jogging20 <621644@qq.com>
2016-08-09 15:30:26 +0800

    感觉没动什么15：20，login,register 框都错位了

--- 百思不得姐g1/百思不得姐g1/classes/notice/views/kqyLoginRegisterViewController.m
+++ 百思不得姐g1/百思不得姐g1/classes/notice/views/kqyLoginRegisterViewController.m
@@ -10,23 +10,46 @@
 
 @interface kqyLoginRegisterViewController ()
 - (IBAction)closeBtn;
-- (IBAction)registerBtn:(id)sender;
+- (IBAction)registerBtn:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *loginBtn;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *leftMargin;
 
 @end
 
+
 @implementation kqyLoginRegisterViewController
+- (IBAction)loginOrRegist:(UIButton *)sender {
+
+    [self.view endEditing:YES];
+    
+    if (self.leftMargin.constant ) {
+        self.leftMargin.constant = 0;
+//        [sender setTitle:@"注册" forState:UIControlStateNormal];
+    } else {
+        self.leftMargin.constant = - self.view.frame.size.width;
+//        [sender setTitle:@"haveed" forState:UIControlStateNormal];
+    }
+    [UIView animateWithDuration:1 animations:^{
+        [self.view layoutIfNeeded];
+    }];
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 //    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
     // Do any additional setup after loading the view from its nib.
+//    self.loginBtn.layer.cornerRadius = 5;
+//    self.loginBtn.layer.masksToBounds = YES;
+    [self.loginBtn setValue:@5 forKeyPath:@"layer.cornerRadius"];
+    [self.loginBtn setValue:@YES forKeyPath:@"layer.masksToBounds"];
 }
 - (UIStatusBarStyle)preferredStatusBarStyle {
     return UIStatusBarStyleLightContent;
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    [self  dismissViewControllerAnimated:YES completion:nil];
+    [self.view endEditing:YES];
     
 }
 
