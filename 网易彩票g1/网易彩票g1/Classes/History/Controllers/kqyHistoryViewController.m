8911a4363e49ee63d81b7295b9271a9a025f2a69
jogging20 <621644@qq.com>
2016-12-22 11:09:28 +0800

    3

--- 网易彩票g1/网易彩票g1/Classes/History/Controllers/kqyHistoryViewController.m
+++ 网易彩票g1/网易彩票g1/Classes/History/Controllers/kqyHistoryViewController.m
@@ -43,9 +43,7 @@
 }
 
 //
-//
-//
-//
+//   
 /*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
