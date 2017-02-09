33fa5ad4b9d90022f8b9f4851a3a842baf8ece45
jogging20 <621644@qq.com>
2017-02-09 19:26:53 +0800

    播放本地的MP3,网上下载的

---
+++ playLocalDownloadMp3/playLocalDownloadMp3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  playLocalDownloadMp3
+//
+//  Created by kqy on 09/02/2017.
+//  Copyright © 2017 KQY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
