2d67648789cfdf8cfd484ee1fccaff65d971a558
jogging20 <621644@qq.com>
2017-03-08 11:54:39 +0800

    2

--- playLocalDownloadMp3/playLocalDownloadMp3/classes/Controller/playerViewController.m
+++ playLocalDownloadMp3/playLocalDownloadMp3/classes/Controller/playerViewController.m
@@ -17,13 +17,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor yellowColor];
+    [self setupUI];
 }
 
 - (void) setupUI {
     UILabel *songNameLbl = [[UILabel alloc] init];
     [self.view addSubview:songNameLbl];
     [songNameLbl mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.mas_equalTo(self.view.mas_top).offset(@50);
+//        make.top.mas_equalTo(self.view.mas_top).offset(@50);
+        make.top.mas_equalTo(self.view.mas_top).offset(50);
         make.centerX.mas_equalTo(self.view.mas_centerX);
         make.width.mas_equalTo(@90);
         make.height.mas_equalTo(@35);
