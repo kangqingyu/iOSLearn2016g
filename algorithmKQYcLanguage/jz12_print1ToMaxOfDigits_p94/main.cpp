50459018a6ea5b936128e4f6c408fe332b76921f
jogging20 <621644@qq.com>
2017-03-28 08:54:03 +0800

    3

--- algorithmKQYcLanguage/jz12_print1ToMaxOfDigits_p94/main.cpp
+++ algorithmKQYcLanguage/jz12_print1ToMaxOfDigits_p94/main.cpp
@@ -13,7 +13,7 @@ void print1ToMaxOfDigit(int n  ) {
     }
     int array[n];
     char *number = new char(n+1);
-}   
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
