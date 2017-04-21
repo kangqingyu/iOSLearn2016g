03aa3dc9b2bc478f9f93c7b9d8316e620f53c7ed
jogging20 <621644@qq.com>
2017-04-21 10:28:31 +0800

    8

---
+++ algorithmKQYcLanguage/jz30_KLeastNumbers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  jz30_KLeastNumbers
+//
+//  Created by kqy on 20/04/2017.
+//  Copyright © 2017 kqy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ algorithmKQYcLanguage/jz31_greateNumberOfSubArrays/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  jz31_greateNumberOfSubArrays
+//
+//  Created by kqy on 20/04/2017.
+//  Copyright © 2017 kqy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
