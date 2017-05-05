3f1d8b145608324209187eec36a8bb71b114ff1c
jogging20 <621644@qq.com>
2017-05-05 11:35:46 +0800

    8

---
+++ algorithmKQYcLanguage/jz32_NumberOf1_second/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  jz32_NumberOf1_second
+//
+//  Created by kqy on 05/05/2017.
+//  Copyright © 2017 kqy. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int numberOf1_method1(int number) {
+    
+}
+int numberOf1_method2(int number) {
+    
+}.
+
+int main(int argc, const char * argv[]) {
+    cout << numberOf1_method2(5134) << endl;
+    cout << numberOf1_method1(5134) << std::endl;// method 1
+    
+    return 0;
+}
