60a76b4e7aed198e82d1dec99547792c25a518c2
jogging20 <621644@qq.com>
2017-05-09 13:23:04 +0800

    8

--- algorithmKQYcLanguage/jz38_numberOfK_second/main.cpp
+++ algorithmKQYcLanguage/jz38_numberOfK_second/main.cpp
@@ -8,7 +8,51 @@
 
 #include <iostream>
 using namespace std;
-int getFirstLoc(int *array, int low, int high, int length, int target) {
+int getFirstLoc(int *array, int target, int low, int high) {
+    if (low > high) {
+        return -1;
+    }
+    int middle = (low + high) >> 1;
+    while (low < high) {
+        if (array[middle] == target) {
+            if (array[middle -1] != target || (middle == low)) {
+                return middle;
+            } else high = middle - 1;
+        } else if (array[middle] > target) {
+            high = middle - 1;
+        } else {
+            low = middle + 1;
+        }
+        middle = getFirstLoc(array, target, low, high);
+    }
+    return middle;
+}
+int getBackLoc(int *array, int target, int low, int high) {
+    if (low > high) return -1;
+    int middle = (low + high) >> 1;
+    while (low < high) {
+        if (array[middle] == target ) {
+            if ( array[middle +1] != target || middle == high ) return middle;
+            else low = middle + 1;
+        } else if (array[middle] > target) high = middle - 1;
+        else low = middle + 1;
+        middle = getBackLoc(array, target, low, high);
+    }
+    return middle;
+}
+int numberOfK(int *array, int length, int target) {
+    int low = getFirstLoc(array, target, 0, length - 1);
+    int high = getBackLoc(array, target, 0, length - 1);
+    return high - low  + 1;
+}
+int main(int argc, const char * argv[]) {
+    int array[10] = {1, 2, 3, 4, 4, 5, 5, 5, 5, 6};
+    cout << getFirstLoc(array,  4, 0, 9) <<" " <<getBackLoc(array,   4, 0, 9 ) << endl;
+    cout << numberOfK(array, 10, 4);
+    return 0;
+}
+
+int getFirstLoc0504wrong(int *array, int low, int high, int length, int target) {
     if (low == high) {
         return low;
     }
@@ -23,9 +67,9 @@ int getFirstLoc(int *array, int low, int high, int length, int target) {
     } else if (array[middle] < target) {
         low = middle + 1;
     }
-    return getFirstLoc(array, low, high, length, target);
+    return getFirstLoc0504wrong(array, low, high, length, target);
 }
-int getBackLoc(int *array, int low, int high, int length, int target) {
+int getBackLoc0504wrong(int *array, int low, int high, int length, int target) {
     if (low == high) {
         return low;
     }
@@ -38,16 +82,10 @@ int getBackLoc(int *array, int low, int high, int length, int target) {
         } else low = middle + 1;
     } else if (array[middle] > target) high = middle - 1;
     else low = middle + 1;
-    return getBackLoc(array, low, high, length, target);
+    return getBackLoc0504wrong(array, low, high, length, target);
 }
-int numberOfK(int *array, int length, int target) {
-    int front = getFirstLoc(array, 0, length -1, length, target);
-    int back = getBackLoc(array, 0, length - 1, length, target);
+int numberOfK0504wrong(int *array, int length, int target) {
+    int front = getFirstLoc0504wrong(array, 0, length -1, length, target);
+    int back = getBackLoc0504wrong(array, 0, length - 1, length, target);
     return back - front + 1;
-}
-int main(int argc, const char * argv[]) {
-    int array[10] = {1, 2, 3, 4, 5, 5, 5, 5, 5, 6};
-    cout << getFirstLoc(array, 10,0, 9, 5) <<" " <<getBackLoc(array, 10, 0, 9, 5) << endl;
-    cout << numberOfK(array, 10, 5);
-    return 0;
 }
