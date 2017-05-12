dc6791f53979b6e7433b35e66de32f77e8cc8472
jogging20 <621644@qq.com>
2017-05-12 11:42:02 +0800

    3

---
+++ cppPrimer/exampleTest/chapter2/46p.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int ival = 1024;
+   int &refIval = ival;
+   //int &refIval2;
+
+   return 0;
+}
---
+++ cppPrimer/exampleTest/chapter2/76p.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+   string s5 = "hiya";
+   string s6("hiya");
+   string s7(10, 'c'); 
+   cout << s5 << " " << s6 << " " << s7 << " " << endl;
+   return 0;
+}
